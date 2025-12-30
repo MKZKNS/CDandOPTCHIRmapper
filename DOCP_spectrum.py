e5be621487cc8f4ea0394a691a38857518cc4cbf
MKZKNS <ahint1ability102@gmail.com>
2025-12-30 14:42:12 +0900

    add: auto unit change, max docp writer

--- DOCP_spectrum.py
+++ DOCP_spectrum.py
@@ -42,8 +42,11 @@ def DOCP_spectrum(Ex, Ey, Phix, Phiy):
 
 if __name__ == "__main__":
     # import E, Phi from csv files
-    filename = r"D:\研究データ\RA発表会\Transmission_Y.csv"
+    filename = r"D:\研究データ\20251230_thzBEshift_woBowtie\circle_aperture_Ypol.csv"
     data = np.loadtxt(filename, delimiter=',', skiprows=1)
+    head = np.loadtxt(filename, delimiter=',', max_rows=1, dtype=str)
+    # pickup frequency unit
+    freq_unit = head[0].split('[')[1].split(']')[0]
     frequency = data[:, 0]
     Ex = data[:, 1]
     Ey = data[:, 3]
@@ -56,7 +59,7 @@ if __name__ == "__main__":
     figname = os.path.join(dirname, 'E_spectrum.png')
     plt.plot(frequency, Ex, label='Ex')
     plt.plot(frequency, Ey, label='Ey')
-    plt.xlabel('Frequency (GHz)')
+    plt.xlabel(f'Frequency [{freq_unit}]')
     plt.ylabel('Transmission')
     plt.title('Electric Field Components')
     plt.legend()
@@ -68,7 +71,7 @@ if __name__ == "__main__":
     figname = os.path.join(dirname, 'Phase_spectrum.png')
     plt.plot(frequency, Phix, label=r'$\phi_x$')
     plt.plot(frequency, Phiy, label=r'$\phi_y$')
-    plt.xlabel('Frequency (GHz)')
+    plt.xlabel(f'Frequency [{freq_unit}]')
     plt.ylabel('Phase (degrees)')
     plt.title('Phase of Electric Field Components')
     plt.legend()
@@ -79,9 +82,18 @@ if __name__ == "__main__":
 
     figname = os.path.join(dirname, 'DOCP_spectrum.png')
     plt.plot(frequency, DOCP)
-    plt.xlabel('Frequency (GHz)')
+    # max vline
+    idx_max_docp = np.argmax(np.abs(DOCP))
+    plt.vlines(frequency[idx_max_docp], -1, 1, colors='red', linestyles='dashed', linewidth=3)
+    # text box
+    plt.text(0.05, 0.05, f'MAX DOCP: {DOCP[idx_max_docp]:.2f}\nFreq: {frequency[idx_max_docp]:.2f} {freq_unit}',
+             backgroundcolor='lightblue',
+             fontsize="x-large",
+             transform=plt.gca().transAxes)
+    plt.xlabel(f'Frequency [{freq_unit}]')
     plt.ylabel('DOCP')
     plt.ylim(-1, 1)
+
     plt.title('Degree of Circular Polarization Spectrum')
     plt.grid()
     plt.savefig(figname)
